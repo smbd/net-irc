4034044189765f92d7a316ef4782329bc276a57e
smbd <smbd.jp@gmail.com>
2009-12-19 02:34:33 +0900

    別のが混ざっていたのでやりなおし
    Revert "followerのidの配列が標準出力にダンプされているので出ないようにした"
    
    This reverts commit a81cb0c735a5445193cb3d12661f410f6ca16dda.

--- examples/tig.rb
+++ examples/tig.rb
@@ -1432,6 +1432,7 @@ class TwitterIrcGateway < Net::IRC::Server::Session
 
 	def check_friends
 		@follower_ids = page("followers/ids/#{@me.id}", :ids)
+		p @follower_ids
 
 		if @friends.nil?
 			@friends = page("statuses/friends/#{@me.id}", :users)
@@ -1606,7 +1607,7 @@ class TwitterIrcGateway < Net::IRC::Server::Session
 
 		raise "github API changed?" unless latest
 
-		is_in_local_repos = system("git rev-parse --verify #{latest} > /dev/null 2>&1")
+		is_in_local_repos = system("git rev-parse --verify #{latest} 2>/dev/null")
 		unless is_in_local_repos
 			current  = commits.map {|i| i['id'] }.index(server_version)
 			messages = commits[0..current].map {|i| i['message'] }
